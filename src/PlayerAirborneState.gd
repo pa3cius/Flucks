9d4dfa3f5934e2ce182851adce1500b9f605bb34
Pat <pv222fz@student.lnu.se>
2024-12-05 16:00:59 +0100

    changed playerAirborneState

--- src/PlayerAirborneState.gd
+++ src/PlayerAirborneState.gd
@@ -3,7 +3,7 @@ class_name PlayerAirborneState extends BaseState
 var double_jumped = false
 
 func enter_state() -> void:
-	pass
+	double_jumped = false
 	# parent.animation_player.play("")
 
 func exit_state() -> void:
@@ -21,4 +21,7 @@ func process_state(delta : float) -> void:
 			state_machine.transition_to_state(state_machine.parent_enum.PLAYERIDLESTATE)
 	var direction := Input.get_axis("player_left", "player_right")
 	parent.velocity = Vector2(parent.SPEED * direction * delta, parent.velocity.y)
+	if Input.is_action_just_pressed("player_jump") and !double_jumped:
+		parent.velocity.y = parent.JUMP_STRENGTH * -1 
+		double_jumped = true
 	parent.move_and_slide()
